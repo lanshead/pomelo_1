b488d526b926b2b3c2c3343af24ab1f5264506ac
lanshead <biolifelanshead@gmail.com>
2023-09-29 14:23:56 +0300

    added CSS

--- pomelo/test.py
+++ pomelo/test.py
@@ -1,4 +1,14 @@
-f = 5 / 9 * (int(input().split('_')[0]) - 32)
-c = 9 / 5 * int(input().split('_')[0]) + 32
+lst1 = list(map(int, input().split(':')))
+lst2 = list(map(int, input().split(':')))
+sec = lst2[2] - lst1[2]
+m = 0
+if sec < 0:
+    sec = 60 + sec
+    m = -1
 
-print(f, c, sep='\n')
+mn = lst2[1] - lst1[1] + m
+if mn < 0:
+    mn = 60 + mn
+    lst2[0] -= 1
+
+print(lst2[0] - lst1[0], mn, sec)
